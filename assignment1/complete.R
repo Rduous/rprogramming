8f0c3f79c0eaa52b8cb6e41a1a215a305888264d
Rduous <git@Rduous.e4ward.com>
2015-01-09 17:36:30 -0700

    Fixing problem.
    Duh.

--- assignment1/complete.R
+++ assignment1/complete.R
@@ -1,13 +1,13 @@
 source("helpers.R")
 
 complete <- function(directory, id = 1:332) {
-  result <- data.frame(id=numeric(), nob=numeric())
+  result <- data.frame(id=numeric(), nobs=numeric())
   frameList <- lapply(id, getResultRow, directory = directory)
   return(Reduce(function(x,y) {rbind(x,y)}, frameList)) 
 }
 
 getResultRow <- function(i, directory) {
   complete <- readFiles(directory, i, complete.cases)
-  newFrame <- data.frame(id=i, nob=nrow(complete))
+  newFrame <- data.frame(id=i, nobs=nrow(complete))
   return(newFrame)
 }
