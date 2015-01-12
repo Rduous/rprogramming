e4d8bf29d3c878596241c569dea0152da2ddae7e
Rduous <git@Rduous.e4ward.com>
2015-01-12 15:39:35 -0700

    Simplifying part 1

--- assignment1/pollutantmean.R
+++ assignment1/pollutantmean.R
@@ -2,8 +2,5 @@ source("helpers.R", local=TRUE)
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   csv <- readFiles(directory, id)
-  good <- ! is.na(csv[,pollutant])
-  values <- csv[good,pollutant]
-  #print(values)
-  return(mean(values))
+  return(mean(csv[,pollutant], na.rm=T))
 }
