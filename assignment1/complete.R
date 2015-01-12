43ddc0fba91bc75e267db8d72c9cd5119b3933ad
Rduous <git@Rduous.e4ward.com>
2015-01-12 15:54:35 -0700

    complete.R deals with frames in smarter way

--- assignment1/complete.R
+++ assignment1/complete.R
@@ -1,13 +1,7 @@
 source("helpers.R")
 
 complete <- function(directory, id = 1:332) {
-  result <- data.frame(id=numeric(), nobs=numeric())
-  frameList <- lapply(id, getResultRow, directory = directory)
-  return(Reduce(function(x,y) {rbind(x,y)}, frameList)) 
-}
-
-getResultRow <- function(i, directory) {
-  complete <- readFiles(directory, i, complete.cases)
-  newFrame <- data.frame(id=i, nobs=nrow(complete))
-  return(newFrame)
+  nobs <- sapply(id, function(x, directory) {nrow(readFiles(directory, x, complete.cases))}, directory=directory)
+  result <- data.frame(id, nobs)
+  return(result)
 }
