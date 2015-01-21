d15378d7a49db4ecab3f183c5e1d3ea224b38a0f
Rduous <git@Rduous.e4ward.com>
2015-01-21 12:40:03 -0700

    Create sortByOutcome.R
    Extracted code from best.R into a reusable format where the state variable is optional.

---
+++ assignment3/sortByOutcome.R
@@ -0,0 +1,27 @@
+outcomeNames <- c("heart attack", "heart failure", "pneumonia")
+outcomeCols <- c(11, 17, 23)
+
+sortByOutcome <- function(state=NULL, outcome) {
+
+    ## Read outcome data
+    outcomes <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available")
+
+    ## Check that outcome is valid
+    
+    outcomeTypeIndex <- match(outcome, outcomeNames)
+   
+    if (is.null(outcome) || is.na(outcome) || is.na(outcomeTypeIndex) ) stop("invalid outcome")
+
+    ## Return hospital name in that state with lowest 30-day death rate
+    col <- outcomeCols[outcomeTypeIndex]
+    # filter by state
+    if (! is.null(state)) {
+         if ( is.na(state) || sum(outcomes[,7] == state) == 0 ) stop("invalid state")
+        outcomes <- outcomes[(outcomes[,7] == state),]
+    }
+    # cast row as numeric, for numeric sort
+    outcomes[,col] <- as.numeric(outcomes[,col])
+    # sort
+    sorted <- outcomes[ order(outcomes[,col],outcomes[,7]),]
+    sorted
+}
